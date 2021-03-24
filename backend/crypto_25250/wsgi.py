760348ec189dfe6d4c2e1682bc616a62b10f008e
Crowdbotics <team@crowdbotics.com>
2021-03-24 19:47:55 +0000

    Update models

--- backend/crypto_25250/wsgi.py
+++ backend/crypto_25250/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_25250.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crypto_25250.settings")
 
 application = get_wsgi_application()
